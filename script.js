0358f9b824f112158a16360183ce94fb35968ba0
Valik148824888 <71930492+Valik148824888@users.noreply.github.com>
2023-03-07 18:57:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function goToSide() {
+    window.location.href = 'buy_page.html'
+}
+
+
+const goToPage = () => {
+    window.location.href = "buy_page.html";
+}
+
+const reload = () => {
+    location.reload = "index.html"
+}
